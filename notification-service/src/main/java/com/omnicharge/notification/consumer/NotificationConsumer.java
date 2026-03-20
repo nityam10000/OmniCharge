e8c0fdddda7905822ad13818905d66c2b4ef47d3
Akshat <hiddenakshat@gmail.com>
2026-03-20 12:01:44 +0530

    Added notification service

--- notification-service/src/main/java/com/omnicharge/notification/consumer/NotificationConsumer.java
+++ notification-service/src/main/java/com/omnicharge/notification/consumer/NotificationConsumer.java
@@ -2,6 +2,7 @@ package com.omnicharge.notification.consumer;
 
 import com.omnicharge.notification.dto.NotificationEvent;
 import com.omnicharge.notification.service.EmailService;
+import com.omnicharge.notification.service.WhatsAppService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +10,12 @@ import org.springframework.stereotype.Component;
 public class NotificationConsumer {
 
     private final EmailService emailService;
+    private final WhatsAppService whatsAppService;
 
-    public NotificationConsumer(EmailService emailService) {
+    public NotificationConsumer(EmailService emailService,
+                                WhatsAppService whatsAppService) {
         this.emailService = emailService;
+        this.whatsAppService = whatsAppService;
     }
 
     @RabbitListener(queues = "notification_queue")
@@ -20,8 +24,15 @@ public class NotificationConsumer {
         System.out.println("📩 Notification Received:");
         System.out.println("Message: " + event.getMessage());
         System.out.println("Email: " + event.getEmail());
+        System.out.println("Phone: " + event.getPhoneNumber());
 
-
+        // ✅ Email send
         emailService.sendEmail(event.getEmail(), event.getMessage());
+
+        // ✅ WhatsApp send
+        whatsAppService.sendWhatsAppMessage(
+                event.getPhoneNumber(),
+                event.getMessage()
+        );
     }
 }
