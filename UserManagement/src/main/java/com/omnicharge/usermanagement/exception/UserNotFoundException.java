91ff20f3287f52af2f57546e0513b780e4154832
Nityam Salgotra <114255270+nityam10000@users.noreply.github.com>
2026-03-23 14:23:18 +0530

    Delete UserManagement directory

--- UserManagement/src/main/java/com/omnicharge/usermanagement/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.omnicharge.usermanagement.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
