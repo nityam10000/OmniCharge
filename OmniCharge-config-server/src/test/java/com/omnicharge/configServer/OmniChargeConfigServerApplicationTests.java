debe964ce38b82467ca459ec5c21320f78c74212
Nityam Salgotra <114255270+nityam10000@users.noreply.github.com>
2026-03-23 15:25:58 +0530

    Add files via upload

---
+++ OmniCharge-config-server/src/test/java/com/omnicharge/configServer/OmniChargeConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnicharge.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniChargeConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
