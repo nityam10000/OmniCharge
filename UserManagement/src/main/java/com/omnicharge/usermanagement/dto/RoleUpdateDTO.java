36795d5d651946db6119dbe11445d1154c5912a5
Nityam Salgotra <114255270+nityam10000@users.noreply.github.com>
2026-03-23 14:14:36 +0530

    Delete UserManagement directory

--- UserManagement/src/main/java/com/omnicharge/usermanagement/dto/RoleUpdateDTO.java
+++
@@ -1,9 +0,0 @@
-package com.omnicharge.usermanagement.dto;
-
-import com.omnicharge.usermanagement.enums.Roles;
-import lombok.Data;
-
-@Data
-public class RoleUpdateDTO {
-    private Roles role;
-}
